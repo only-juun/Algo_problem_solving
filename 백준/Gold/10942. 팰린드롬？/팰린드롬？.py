278ca4425d780ef715ea95816c7ae52525f9333e
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-03-23 09:16:53 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1196 ms, Memory: 258100 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -4,6 +4,11 @@
 # 각 질문은 s, e: s번째부터 e번째 까지 팰린드롬인지(거꾸로=제대로)
 # 대답은 1(yes) or 0(no)
 
+# dp[i][j] := i부터 j까지의 숫자가 회문인지(0 or 1)
+# 아이디어: 새로 추가되는 부분과 첫번째를 비교하고, 나머지 가운데 부분이 회문인지 판단
+# 0~n까지 가면 배열 채우는 것이 불가능
+# 반대로 n~0까지 거꾸로 가면 배열 채우기 가능
+
 import sys
 input = sys.stdin.readline
 
@@ -12,18 +17,16 @@ nums = list(map(int, input().split()))
 m = int(input())
 questions = [tuple(map(int, input().split())) for _ in range(m)]
 
-dp = [[0 for _ in range(n)] for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
 for i in range(n):
     dp[i][i] = 1
 
 for i in range(n - 2, -1, -1):
     for j in range(n - 1, i, -1):
-        if i == j:
-            continue
-        if abs(i - j) == 1:
-            dp[i][j] = 1 if (nums[i] == nums[j]) else 0
+        if (j - i) == 1:
+            dp[i][j] = 1 if nums[i] == nums[j] else 0
         else:
-            dp[i][j] = 1 if ((nums[i] == nums[j]) and (dp[i + 1][j - 1])) else 0
+            dp[i][j] = 1 if nums[i] == nums[j] and dp[i + 1][j - 1] else 0
 
 for s, e in questions:
     print(dp[s - 1][e - 1])
