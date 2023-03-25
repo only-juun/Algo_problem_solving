4b04d92ca77868722a64e6df31bb5504e84ccf3c
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-03-25 16:10:25 +0900

    [Gold V] Title: 암호코드, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -1,29 +1,26 @@
-# boj 2011. 암호코드
 import sys
 
 MOD = 1000000
-n = input()
 
-def a_to_i(num):
-    return 1 <= int(num) <= 9
+password = [0] + list(map(int, input()))
+dp = [0] * len(password)
+dp[0] = 1
+dp[1] = 1 if password[1] != 0 else 0
+
+if not dp[1]:
+    print(0)
+    sys.exit()
 
 def j_to_z(num):
-    return 10 <= int(num) <= 26
+    return 10 <= num <= 26
 
-dp = [0] * 5000
-dp[0] = 1 if a_to_i(n[0]) else 0
-if len(n) == 1:
-    print(dp[len(n) - 1])
-    sys.exit()
-dp[1] = dp[0] if a_to_i(n[1]) else 0
-if j_to_z(n[:2]):
-    dp[1] += 1
-
-for i in range(2, len(n)):
-    if a_to_i(n[i]):
+for i in range(2, len(password)):
+    if password[i] != 0:
         dp[i] += dp[i - 1]
-    if j_to_z(n[i - 1: i + 1]):
+
+    num = password[i-1] * 10 + password[i]
+    if j_to_z(num):
         dp[i] += dp[i - 2]
     dp[i] %= MOD
 
-print(dp[len(n) - 1])
+print(dp[len(password) - 1])
