3dfa85dd8965d234d19513e64186f42a87780bd8
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-03-28 17:55:15 +0900

    [Gold V] Title: 합분해, Time: 1120 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,17 @@
+# boj 2225. 합분해
+MOD = 1000000000
+n, k = tuple(map(int, input().split()))
+
+# dp[i][j] := 지금까지 i개의 정수를 선택했고, 그 합이 j가 되는 경우의 수
+
+dp = [[0] * (n + 1) for _ in range(k + 1)]
+for i in range(n + 1):
+    dp[1][i] = 1
+
+for i in range(1, k + 1):
+    for j in range(n + 1):
+        for x in range(0, j + 1):
+            # print(i - 1, x, dp[i - 1][x])
+            dp[i][j] = (dp[i][j] + dp[i - 1][x]) % MOD
+
+print(dp[k][n])
