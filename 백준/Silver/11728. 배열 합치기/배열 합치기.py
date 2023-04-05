7a435abd7f8dce8b2d65e50f4539abe8aa6d252f
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-04-05 09:57:58 +0900

    [Silver V] Title: 배열 합치기, Time: 1268 ms, Memory: 329548 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -5,11 +5,18 @@ n, m = tuple(map(int, input().split())) # n: a 배열의 크기, m: b 배열의 
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-arr = []
-for i in a:
-    arr.append(i)
-for i in b:
-    arr.append(i)
+a.sort()
+b.sort()
+a.append(10**9 + 1)
+b.append(10**9 + 1)
 
-arr.sort()
-print(*arr)
+a_pointer = 0
+b_pointer = 0
+
+while a_pointer < n or b_pointer < m:
+    if a[a_pointer] <= b[b_pointer]:
+        print(a[a_pointer], end = ' ')
+        a_pointer += 1
+    else:
+        print(b[b_pointer], end = ' ')
+        b_pointer += 1
