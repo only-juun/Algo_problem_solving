ad2ce5e359b624d24e8125011d4503f0a6f6dd04
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-07-28 00:23:08 +0900

    [Gold III] Title: 문자판, Time: 1440 ms, Memory: 129412 KB -BaekjoonHub

--- 백준/Gold/2186. 문자판/문자판.py
+++ 백준/Gold/2186. 문자판/문자판.py
@@ -1,41 +1,36 @@
 import sys
 input = sys.stdin.readline
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-def dfs(x, y, idx):
-    if idx == len(word):
-        return 1
-    if c[x][y][idx] != -1:
-        return c[x][y][idx]
-
-    c[x][y][idx] = 0
-    for i in range(4):
-        temp_x, temp_y = x, y
-        for _ in range(k):
-            nx = temp_x + dx[i]
-            ny = temp_y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if a[nx][ny] == word[idx]:
-                    c[x][y][idx] += dfs(nx, ny, idx+1)
-            temp_x, temp_y = nx, ny
-    return c[x][y][idx]
-
-n, m, k = map(int, input().split())
-a = []
-for _ in range(n):
-    a.append(list(input().strip()))
-word = list(input().strip())
-
+n,m,k = list(map(int,input().strip().split()))
+arr = []
+for i in range(n):
+    row = list(input().strip())
+    arr.append(row)
+cmp = list(input().strip())
 start = []
 for i in range(n):
     for j in range(m):
-        if a[i][j] == word[0]:
-            start.append([i, j])
+        if arr[i][j] == cmp[0]:
+            start.append((i,j))
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+dp = [[[-1]*len(cmp) for i in range(m)] for i in range(n)]
+ans = 0
+def dfs(x,y,depth):
+    global ans
+    if dp[x][y][depth] != -1:
+        return dp[x][y][depth]
+    if depth == len(cmp)-1:
+        return 1
+    dp[x][y][depth] = 0
+    for i in range(1,k+1):
+        for j in range(4):
+            nx = x + i * dx[j]
+            ny = y + i * dy[j]
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == cmp[depth+1]:
+                    dp[x][y][depth] += dfs(nx,ny,depth+1)
+    return dp[x][y][depth]
 
-ans =  0
-c = [[[-1] * len(word) for _ in range(m)] for _ in range(n)]
-for i in range(len(start)):
-    x, y = start[i]
-    ans += dfs(x, y, 1)
+for st in start:
+    x,y = st
+    ans += dfs(x,y,0)
 print(ans)
