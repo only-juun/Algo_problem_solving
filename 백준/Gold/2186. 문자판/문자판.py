199f1a2eff3b66b8d872b017bb0a98f8867c2be1
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-07-28 00:45:30 +0900

    [Gold III] Title: 문자판, Time: 1680 ms, Memory: 130820 KB -BaekjoonHub

--- 백준/Gold/2186. 문자판/문자판.py
+++ 백준/Gold/2186. 문자판/문자판.py
@@ -1,36 +1,52 @@
 import sys
 input = sys.stdin.readline
-n,m,k = list(map(int,input().strip().split()))
-arr = []
-for i in range(n):
-    row = list(input().strip())
-    arr.append(row)
-cmp = list(input().strip())
-start = []
+
+# 입력받기
+n, m, k = list(map(int, input().strip().split()))
+board = [list(input().strip()) for _ in range(n)]
+word = input().strip()
+
+dxs = [-1,0,1,0]
+dys = [0,1,0,-1]
+dp = [[[-1] * len(word) for i in range(m)] for i in range(n)]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def can_go(x, y, cur):
+    return in_range(x, y) and board[x][y] == word[cur]
+            
+def dfs(x, y, cur):
+    global answer
+
+    # 이미 방문한 곳인 경우
+    if dp[x][y][cur] != -1:
+        return dp[x][y][cur]
+    
+    # 단어를 완성한 경우
+    if cur == len(word) - 1:
+        return 1
+    
+    # 필요한 문자가 아닌 경우
+    if board[x][y] != word[cur]:
+        return 0
+	
+    # 상하좌우 k거리 떨어진 곳 전부 탐색
+    cnt = 0
+    for i in range(1, k + 1):
+        for dx, dy in zip(dxs, dys):
+            nx = x + i * dx
+            ny = y + i * dy
+            if can_go(nx, ny, cur + 1):
+                cnt += dfs(nx, ny, cur + 1)
+    dp[x][y][cur] = cnt
+
+    return dp[x][y][cur]
+
+answer = 0
 for i in range(n):
     for j in range(m):
-        if arr[i][j] == cmp[0]:
-            start.append((i,j))
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-dp = [[[-1]*len(cmp) for i in range(m)] for i in range(n)]
-ans = 0
-def dfs(x,y,depth):
-    global ans
-    if dp[x][y][depth] != -1:
-        return dp[x][y][depth]
-    if depth == len(cmp)-1:
-        return 1
-    dp[x][y][depth] = 0
-    for i in range(1,k+1):
-        for j in range(4):
-            nx = x + i * dx[j]
-            ny = y + i * dy[j]
-            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == cmp[depth+1]:
-                    dp[x][y][depth] += dfs(nx,ny,depth+1)
-    return dp[x][y][depth]
+        if board[i][j] == word[0]:
+            answer += dfs(i, j, 0)
 
-for st in start:
-    x,y = st
-    ans += dfs(x,y,0)
-print(ans)
+print(answer)
