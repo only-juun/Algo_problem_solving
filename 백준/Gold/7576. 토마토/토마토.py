582f61b64c8e5d395a86affe0d0a4cb02d55d07a
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-07-29 18:00:00 +0900

    [Gold V] Title: 토마토, Time: 640 ms, Memory: 172808 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,49 +1,42 @@
-# boj 7576.토마토
 import sys
 from collections import deque
-input = sys.stdin.readline
+from typing import List, Tuple
 
-# 입력받기
-m, n = tuple(map(int, input().split()))
-arr = [list(map(int, input().split())) for _ in range(n)]
 
-# 범위 내에 있는지 확인
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < m
+def input() -> str:
+    return sys.stdin.readline().rstrip()
 
-# 익지 않은 토마토인지 확인
-def can_go(x, y):
-    return in_range(x, y) and arr[x][y] == 0
 
-# 모든 토마토가 익었는지 확인
-def all_tomatoes_ripe():
-    for i in range(n):
-        for j in range(m):
-            if arr[i][j] == 0:
-                return False
-    return True
+Dy: Tuple[int, ...] = (-1, 1, 0, 0)
+Dx: Tuple[int, ...] = (0, 0, -1, 1)
 
-def bfs():
-    days = 0
-    dxs, dys = [1, 0, -1, 0], [0, 1, 0, -1]
-    while q:
-        x, y, cnt = q.popleft()
-        
-        days = max(days, cnt)
+M, N = map(int, input().split())  # type: (int, int)
+box: List[List[int]] = [list(map(int, input().split())) for i in range(N)]
 
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            if can_go(nx, ny):
-                arr[nx][ny] = 1
-                q.append((nx, ny, cnt + 1))
-    return days
 
-q = deque()
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == 1:
-            q.append((i, j, 0))
+def get_position_of_tomato() -> List[Tuple[int, int]]:
+    return [(y, x) for y in range(N) for x in range(M) if box[y][x] == 1]
 
-answer = bfs()
 
-print(answer if all_tomatoes_ripe() else -1)
+def check_all_tomato_riped() -> bool:
+    return len([1 for y in range(N) for x in range(M) if box[y][x] == 0]) == 0
+
+
+Q = deque(get_position_of_tomato())
+
+step: int = -1
+while Q:
+    qsize: int = len(Q)
+    step += 1
+    for _ in range(qsize):
+        y, x = Q.popleft()  # type: (int, int)
+        for dy, dx in zip(Dy, Dx):  # type: (int, int)
+            nexty, nextx = y + dy, x + dx  # type: (int, int)
+            if not (0 <= nexty < N and 0 <= nextx < M):
+                continue
+            if box[nexty][nextx] != 0:
+                continue
+            box[nexty][nextx] = 1
+            Q.append((nexty, nextx))
+
+print(step if check_all_tomato_riped() else -1)
