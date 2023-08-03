88033465ffa90026f5cbd2339f0eb169afc70259
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-08-03 12:47:18 +0900

    [Silver I] Title: 안전 영역, Time: 616 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,56 +1,37 @@
 import sys
 from collections import deque
+
+def bfs(x, y, h, visited):
+    queue = deque([(x, y)])
+    visited[x][y] = True  # 방문 처리
+
+    while queue:
+        cx, cy = queue.popleft()
+
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and arr[nx][ny] > h:
+                queue.append((nx, ny))
+                visited[nx][ny] = True
+
+
 input = sys.stdin.readline
 
-n = int(input())
-arr = []
-min_height, max_height = sys.maxsize, -1
-for _ in range(n):
-    row = list(map(int, input().split()))
-    min_height, max_height = min(min(row), min_height), max(max(row), max_height)
-    arr.append(row)
-visited = [[False for _ in range(n)] for _ in range(n)]
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+max_height = max(map(max, arr))  # 최대 높이 구하기
 
-# 방문 여부 배열 초기화
-def initialize():
-    for i in range(n):
-        for j in range(n):
-            visited[i][j] = False
+answer = 0
+for i in range(max_height):  # 높이마다 안전구역 갯수 비교
+    visited = [[False] * N for _ in range(N)]  # 높이가 바뀔 때 마다 방문배열 초기화
+    safe_zones = 0
 
-# 배열 범위 확인
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
+    for j in range(N):
+        for k in range(N):
+            if not visited[j][k] and arr[j][k] > i:
+                bfs(j, k, i, visited)
+                safe_zones += 1
 
-# 진행 가능한 칸인지 확인
-def can_go(x, y, height):
-    return in_range(x, y) and arr[x][y] > height and not visited[x][y]
-
-# 영역 탐색
-def bfs(height):
-    dxs, dys = [0, 1, 0, -1], [1, 0, -1, 0]
-
-    while q:
-        x, y = q.popleft()
-
-        for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            if can_go(nx, ny, height):
-                visited[nx][ny] = True
-                q.append((nx, ny))
-
-answer = 1
-q = deque()
-for height in range(min_height, max_height):
-    cnt = 0
-    for i in range(n):
-        for j in range(n):
-            # 한번 탐색을 진행할 때마다 1개의 영역을 체크할 수 있음
-            if arr[i][j] > height and not visited[i][j]:
-                visited[i][j] = True
-                q.append((i, j))
-                bfs(height)
-                cnt += 1
-    initialize()
-    answer = max(cnt, answer)
+    answer = max(answer, safe_zones)
 
 print(answer)
