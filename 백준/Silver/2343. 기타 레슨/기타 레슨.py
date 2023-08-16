342b731c74fa9d4f0b56478682c0172ed807deb5
onlyjoon <79013722+only-juun@users.noreply.github.com>
2023-08-16 16:40:35 +0900

    [Silver I] Title: 기타 레슨, Time: 296 ms, Memory: 42340 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,28 @@
+import sys
+
+n, m = tuple(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+left, right = max(arr), sum(arr)
+ans = right
+
+def is_possible(target):
+    cnt = 1
+    s = 0
+    for i in range(n):
+        if (s + arr[i]) <= target:
+            s += arr[i]
+        else:
+            s = arr[i]
+            cnt += 1
+    return cnt <= m
+
+while left <= right:
+    mid = (left + right) // 2
+
+    if is_possible(mid):
+        ans = min(ans, mid)
+        right = mid - 1
+    else:
+        left = mid + 1
+print(ans)
