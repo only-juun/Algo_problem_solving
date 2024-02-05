d84210c34a10033502f106def128bdaa6dcaf5cd
onlyjoon <79013722+only-juun@users.noreply.github.com>
2024-02-05 20:04:37 +0900

    [Gold IV] Title: N-Queen, Time: 4576 ms, Memory: 119928 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,27 +1,20 @@
 n = int(input())
-
 ans = 0
-row = [0] * n
+left_diagonal = [0] * (2 * n - 1)  # 주 대각선
+right_diagonal = [0] * (2 * n - 1)  # 부 대각선
+column = [0] * n  # 열
 
-def is_promising(x):
-    for i in range(x):
-        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
-            return False
-    
-    return True
-
-def n_queens(x):
+def solve(row):
     global ans
-    if x == n:
+    if row == n:
         ans += 1
         return
+    for col in range(n):
+        if column[col] or left_diagonal[row + col] or right_diagonal[row - col + n - 1]:
+            continue
+        column[col] = left_diagonal[row + col] = right_diagonal[row - col + n - 1] = 1
+        solve(row + 1)
+        column[col] = left_diagonal[row + col] = right_diagonal[row - col + n - 1] = 0
 
-    else:
-        for i in range(n):
-            # [x, i]에 퀸을 놓겠다.
-            row[x] = i
-            if is_promising(x):
-                n_queens(x + 1)
-
-n_queens(0)
+solve(0)
 print(ans)
