c411ea252ee25b8eba3213dc9cb28f6588170856
onlyjoon <79013722+only-juun@users.noreply.github.com>
2024-03-14 00:00:45 +0900

    [Silver III] Title: 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2082. 시계/시계.py
+++ 백준/Silver/2082. 시계/시계.py
@@ -11,36 +11,25 @@ number = {
     9: "####.####..####"
 }
 
-arr = ['' for _ in range(4)]
-for _ in range(5):
-    line = list(input().split())
-    for i in range(4):
-        arr[i] += line[i]
+arr = [''.join(col) for col in zip(*(input().split() for _ in range(5)))]
 
-# 켜지지 않아야 하는 것이 켜진 경우는 없다 
-# 정상 숫자에서 꺼진 부분에 불이 있는 것은 배제
-# 첫번째 자리: 0,1,2 중에서 가능한 숫자 먼저 선택
-# 두번째 자리: 0-9 중에서 가능한 숫자 먼저 선택
-# 세번째 자리: 0-5 중에서 가능한 숫자 먼저 선택
-# 네번째 자리: 0-9 중에서 가능한 숫자 먼저 선택
 def select_number(pos):
     # 자리에서 가능한 숫자들을 순차적으로 탐색
     for origin in range(digits[pos] + 1):
-        flag = True
-        # 비교하면서 origin에서 꺼진 부분이 arr에 켜져있으면 다음 숫자
-        # 아니라면 숫자를 고르고 return
+        # 현재 숫자(origin)가 조건을 만족하는지 검사
         for i in range(15):
+            # 조건을 만족하지 않으면, 즉 number에서 꺼진 부분이 arr에 켜져 있으면
             if number[origin][i] == "." and arr[pos][i] == "#":
-                flag = False
+                # 이 숫자는 사용할 수 없으므로 다음 숫자로 넘어감
                 break
-        if flag:
+        else:
+            # 모든 검사를 통과했다면, 이 숫자는 사용 가능
             return str(origin)
         
-
 answer = ""
 digits = [2, 9, 5, 9]
 for i in range(4):
     # 각 자리에서 가능한 작은 숫자를 선택
     answer += select_number(i)
 
-print(answer[:2] + ":" + answer[2:])
+print(f"{answer[:2]}:{answer[2:]}")
