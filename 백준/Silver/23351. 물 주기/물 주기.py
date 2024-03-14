723dfb839a04af72ce8584b692b3c14b4a1b3682
onlyjoon <79013722+only-juun@users.noreply.github.com>
2024-03-14 15:44:08 +0900

    [Silver III] Title: 물 주기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/23351. 물 주기/물 주기.py
+++ 백준/Silver/23351. 물 주기/물 주기.py
@@ -1,25 +1,18 @@
 n, k, a, b = map(int, input().split())
-# n개 화분, 각 화분의 수분k, 연속된 a개 화분에 물주면 b수분 증가
-# 모든 화분이 1씩 감소
-# 수분 0은 다이
 
 pots = [k for _ in range(n)]
 ans = 1
 while True:
-    # 연속 a개 화분에 물주기
     for i in range(n):
-        if pots[i] == min(pots):
-            for j in range(a):
-                pots[i + j] += b
-            break
-
-    # 모든 수분 1 감소
-    for i in range(n):
+        if i < a:
+            pots[i] += b
         pots[i] -= 1
+        
+    pots.sort()
 
-    if min(pots) == 0:
+    if pots[0] == 0:
         break
-
+        
     ans += 1
 
 print(ans)
