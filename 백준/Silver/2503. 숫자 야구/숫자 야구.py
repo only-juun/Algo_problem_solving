0c4105bbdb379c599dd6ae360563a4e853cdd9a1
onlyjoon <79013722+only-juun@users.noreply.github.com>
2024-03-17 15:29:29 +0900

    [Silver III] Title: 숫자 야구, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.py
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.py
@@ -1,24 +1,25 @@
 import sys
-from itertools import permutations as p
+from itertools import permutations
+
 input = sys.stdin.readline
 
 n = int(input())
 
-arr = list(p('123456789', 3))
+arr = [''.join(permutation) for permutation in permutations('123456789', 3)]
 
 def get_sb(a, num):
     s, b = 0, 0
-    for x, y in zip(list(a), list(num)):
+    for x, y in zip(a, num):
         if x == y:
             s += 1
-        elif x in list(num):
+        elif x in num:
             b += 1
     return (s, b)
 
-
-for i in range(n):
+for _ in range(n):
     num, strike, ball = map(int, input().split())
     temp = []
+    
     for a in arr:
         x, y = get_sb(a, str(num))
         if strike == x and ball == y:
