8a4b14450c6456a73388084d7d6fc01cee851ae1
onlyjoon <79013722+only-juun@users.noreply.github.com>
2024-03-18 16:20:41 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.py
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.py
@@ -1,24 +1,30 @@
-def is_unique(num):
-    bit_mask = 0
-    while num > 0:
-        digit = num % 10
-        if bit_mask & (1 << digit):
-            return False
-        bit_mask |= (1 << digit)
-        num //= 10
-    return True
-
-arr = [0] * 1000001
-count = 1
-num = 1
-while count <= 1000000:
-    if is_unique(num):
-        arr[count] = num
-        count += 1
-    num += 1
-
 while True:
     n = int(input())
     if n == 0:
         break
-    print(arr[n])
+    n -= 1
+    i = 1
+    c = 9
+    while True:
+        if c > n:
+            break
+        n -= c
+        c *= (10 - i)
+        i += 1
+
+    c = 1
+    for j in range(i-1):
+        c *= (9-j)
+    l = []
+    for j in range(i):
+        l.append(n // c)
+        n %= c
+        c //= (9-j)
+    l1 = [l[0] + 1]
+    l2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    l2.remove(l[0]+1)
+    for j in range(i-1):
+        l1.append(l2.pop(l[j+1]))
+    for x in l1:
+        print(x, end="")
+    print()
